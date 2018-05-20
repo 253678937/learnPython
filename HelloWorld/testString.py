37b22b42dac7739d1f173c78fe82010650364995
253678937 <253678937@qq.com>
2018-05-20 16:58:48 +0800

    添加新文件

---
+++ HelloWorld/testString.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#Created by zhouzebing on 2018/5/20.
+
+#字符串可以进行加法运算
+print "aaa" + "bbb"     # aaabbb
+
+#字符串可以进行乘法运算
+print "aaa" * 3         # aaaaaaaaa
---
+++ _ReadMe.py
@@ -0,0 +1,26 @@
+
+# -*- coding: utf-8 -*-
+# Created by zhouzebing on 2018/5/20.
+
+#安装 python
+
+
+#安装 pyCharm
+#注册方法在QQ我的收藏里面
+
+
+#修改 python 解释器版本
+#   Setting -> Project:python -> project interpreter
+
+
+#修改新文件的文件格式
+#   Setting -> Edit -> File and Code Template -> Python Script
+#   在文件开头添加以下内容
+# -*- coding: utf-8 -*-
+# Created by ${USER} on ${DATE}.
+
+
+#新建 python 工程
+#   File -> New Project -> Pure Python
+#选择新工程的路径
+#选择解释器的版本
